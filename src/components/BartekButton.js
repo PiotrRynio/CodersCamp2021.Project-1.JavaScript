b55eed1367bf672f4d007a3de36e08e0183d0c09
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-11-30 20:19:41 +0100

    during work Bartosz

---
+++ src/components/BartekButton.js
@@ -0,0 +1,3 @@
+const Button = () => {
+  const button = document.createElement('button');
+};
--- src/layouts/Wrapper.js
+++ src/layouts/Wrapper.js
@@ -1,7 +1,8 @@
-export const Wrapper = () => {
+const Wrapper = () => {
   const wrapper = document.createElement('div');
   wrapper.classList.add('wrapper');
   wrapper.id = 'wrapper';
 
   return wrapper;
 };
+export default Wrapper;
