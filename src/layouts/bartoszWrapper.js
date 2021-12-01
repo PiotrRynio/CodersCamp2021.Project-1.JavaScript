80667b6251da56d830155ed51a13b015e7a8c1a4
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-01 12:33:33 +0100

    add new wrapper.js

--- src/layouts/bartoszWrapper.js
+++ src/layouts/bartoszWrapper.js
@@ -2,7 +2,6 @@ const Wrapper = () => {
   const wrapper = document.createElement('div');
   wrapper.classList.add('wrapper');
   wrapper.id = 'wrapper';
-
   return wrapper;
 };
 export default Wrapper;
