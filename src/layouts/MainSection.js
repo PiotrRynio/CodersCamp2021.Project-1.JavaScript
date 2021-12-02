37f89be8420c6819e944cb30cf14d5acdcde5b19
Patryk Święickci <patswiecicki@gmail.com>
2021-12-02 10:01:32 +0100

    Move layout to html file.

--- src/layouts/MainSection.js
+++
@@ -1,6 +0,0 @@
-const MainSection = () => {
-  const mainSection = document.createElement('div');
-  mainSection.classList.add('mainSection');
-  return mainSection;
-};
-export default MainSection;
