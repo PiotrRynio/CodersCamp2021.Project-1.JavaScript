5ec33b5daae18d81540541c8c223ead3ba2e903d
Agnieszka Przybylowska <agnieszka.przybylowska@coderscrew.pl>
2021-12-02 15:45:00 +0100

    added tests

--- src/model/checkIfAnswerIsCorrect.test.js
+++ src/model/checkIfAnswerIsCorrect.test.js
@@ -1,8 +1,28 @@
-const {checkIfAnswerIsCorrect} = require("./checkIfAnswerIsCorrect.js"); 
+import { checkIfAnswerIsCorrect } from "./checkIfAnswerIsCorrect.js"; 
 
-test('properly compares two asnwers', ()=>{
-    //expect(checkIfAnswerIsCorrect('A','A').toBeTruthy())
-// expect(true).toBe(false)
-console.log(checkIfAnswerIsCorrect("A","A"))
-}
-)
+test('properly compares two good asnwers', ()=>{
+    expect(checkIfAnswerIsCorrect('A','A')).toBe(1)})
+
+test('properly compares two wrong answers', ()=>{
+    expect(checkIfAnswerIsCorrect('A','B')).toBe(0)})
+
+test('check if both of types are strings',()=>{
+    let A = {}; 
+    expect(checkIfAnswerIsCorrect(A,'A')).toBe(-2)})
+
+test('check if input is not equal null ',()=>{
+    let A = null; 
+    expect(checkIfAnswerIsCorrect(A,'A')).toBe(-2)})
+
+test('check if input is not equal undefined ',()=>{
+    let A = undefined; 
+    expect(checkIfAnswerIsCorrect(A,'A')).toBe(-2)})    
+
+test('check if input is not empty ',()=>{
+    expect(checkIfAnswerIsCorrect()).toBe(-1)}) 
+
+test('check if there are not more than two inputs',()=>{
+    expect(checkIfAnswerIsCorrect('A','B','C')).toBe(-1)}) 
+
+test('check if there are not less than two inputs',()=>{
+    expect(checkIfAnswerIsCorrect('A')).toBe(-1)}) 
