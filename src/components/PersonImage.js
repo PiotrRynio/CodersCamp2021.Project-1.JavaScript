9473620530a1071665aef2731d0dd995431971a9
Jarosław Kaczor <k3.jarek@gmail.com>
2021-12-02 19:27:56 +0100

    add PersonImage component

---
+++ src/components/PersonImage.js
@@ -0,0 +1,9 @@
+const PersonImage = () => {
+    const image = document.createElement('div');
+    image.classList.add("personImage");
+    image.id = "personImage";
+
+    return image;
+};
+
+export default PersonImage;
