afccaa3c63ec8f677a5a72f617f74cd992e9ba0e
kuba jarosz <kuba.jarooosz@gmail.com>
2021-12-05 00:31:37 +0100

    added test (failed)

---
+++ src/model/saveScore.test.js
@@ -0,0 +1,9 @@
+import { saveScore } from './saveScore';
+
+describe('saveScore', () => {
+  it('saves score into local storage', () => {
+        afterEach(() => { 
+            localStorage.clear();
+        })
+    });
+});
