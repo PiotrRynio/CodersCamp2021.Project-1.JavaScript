371c0a079685e3d37189170a1a98ae90e06191ff
Patryk Święickci <patswiecicki@gmail.com>
2021-12-04 10:06:36 +0100

    Add utilities tests and fix randomNumbers function

--- src/model/utilities/utilities.js
+++ src/model/utilities/utilities.js
@@ -1,7 +1,7 @@
 export const randomNumbers = (lowestNumber, highestNumber, returnedArrayLength) => {
   const numbers = [];
   while(numbers.length != returnedArrayLength){
-    const number = Math.floor(Math.random() * (highestNumber + 1)) + lowestNumber;
+    const number = Math.floor(Math.random() * ((highestNumber - lowestNumber + 1)) + lowestNumber);
     if(!numbers.includes(number)){
       numbers.push(number);
     }
