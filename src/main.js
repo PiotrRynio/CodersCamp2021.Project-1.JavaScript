28d085047c9841a9a36bc0590e3c30edeeecd5ec
Patryk Święickci <patswiecicki@gmail.com>
2021-12-04 14:16:05 +0100

    Remove additional lines from main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import './scss/style.scss';
-import questionGeneratator from './model/questionGenerator';
 
 function main() {
-  const askedQuestions = [];
-  console.log(questionGeneratator("characters", askedQuestions));
   document.querySelector('#app');
 }
 
