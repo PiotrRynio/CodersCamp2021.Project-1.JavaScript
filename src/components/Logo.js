8dd47aab08d05b894f291f362d3ce6bf7d0515a1
Patryk Święickci <patswiecicki@gmail.com>
2021-12-05 13:50:46 +0100

    Add functionality to reload on logo click

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,13 @@
+import { doc } from "prettier";
+
 const Logo = () => {
   const logo = document.createElement('img');
   logo.classList.add('logosection__logo');
   logo.src = "logo.png";
   logo.alt = "Breaking Bad logo";
+  logo.onclick = () => {
+    document.location.reload();
+  }
 
   return logo;
 }
