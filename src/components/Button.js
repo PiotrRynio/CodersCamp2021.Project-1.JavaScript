90fdd8aed57786604fdfc4576b8c1d5e3449841b
PiotrWR <pwrynio@gmail.com>
2021-12-05 13:07:13 +0100

    Add test button

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = () => {
-  const wrapper = document.createElement('button');
-  wrapper.classList.add('button');
-  wrapper.id = 'button';
-
-  return wrapper;
-};
-
-export default Button;
--- src/layouts/MainSection.js
+++ src/layouts/MainSection.js
@@ -1,6 +1,11 @@
+import TestButton from '../components/TestButton';
+
 const MainSection = () => {
   const mainSection = document.createElement('section');
   mainSection.classList.add('mainSection');
+
+  mainSection.appendChild(TestButton());
+
   return mainSection;
 };
 export default MainSection;
