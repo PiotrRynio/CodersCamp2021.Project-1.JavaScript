be750aa311f2e7d66620551f22bcf01061ba4794
PSwiecicki <patswiecicki@gmail.com>
2021-12-05 14:24:18 +0100

    feat/question object generation (#34)
    * Add functions to get random id numbers
    
    * Add function to generate question about people
    
    * Update people question generator
    
    * Move generators to difrent files
    
    * Add generators for qoutes and deaths
    
    * Add changes from CR and refactor code
    
    * Add utilities tests and fix randomNumbers function
    
    * Fix import
    
    * Fix problem with async functions in jest
    
    * Add questionGenerator tests
    
    * Remove additional lines from main.js
    
    * Add changes from CR
    
    * Pull files
    
    * Change wrong names

---
+++ src/model/utilities/randomValueFromArray.test.js
@@ -0,0 +1,31 @@
+import randomValueFromArray from './randomValueFromArray';
+
+describe('randomValueFromArray', () => {
+  afterEach(()=> {
+    jest.spyOn(global.Math, 'random').mockRestore();
+  })
+
+  test("If we give it array, thats return it's value.",() => {
+    const testArray = [1, 2, 3, 4];
+    const result = randomValueFromArray(testArray);
+    expect(testArray).toContain(result);
+  })
+
+  test("If random function returns 0 then function should return first value", () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0);
+
+    const testArray = [1, 2, 3, 4];
+    const result = randomValueFromArray(testArray);
+
+    expect(result).toBe(testArray[0]);
+  });
+
+  test("If random function returns almost 1 then function should return last value", () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.999999);
+
+    const testArray = [1, 2, 3, 4];
+    const result = randomValueFromArray(testArray);
+
+    expect(result).toBe(testArray[testArray.length-1]);
+  });
+})
