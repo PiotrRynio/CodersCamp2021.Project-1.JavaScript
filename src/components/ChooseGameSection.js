90af0b21d6f11c88c3d82241e1e5245a58fbb218
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-05 12:13:07 +0100

    fixed layout in JS

---
+++ src/components/ChooseGameSection.js
@@ -0,0 +1,6 @@
+const ChooseGameSection = () => {
+  const chooseGame = document.createElement('div');
+  chooseGame.classList.add('chooseGameSection');
+  return chooseGame;
+};
+export default ChooseGameSection;
