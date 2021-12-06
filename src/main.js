33b012b1459e53e6a68864cc1ed705f65b14650f
jaroslawkaczor <k3.jarek@gmail.com>
2021-12-06 21:20:46 +0100

    refactoring - adding div in mainSection.js instead of index.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import './scss/style.scss';
 import Wrapper from './layouts/MainWrapper';
-import PersonImage from './components/PersonImage';
-
 
 function main() {
   document.querySelector('#app').appendChild(Wrapper());
+
 }
 
 main();
