9cfa0783a77f15c8e70b67badbf0faee7a21f068
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-06 23:50:44 +0100

    add the second test

--- src/components/MainImage.test.js
+++ src/components/MainImage.test.js
@@ -1,9 +1,13 @@
 import MainImage from "./MainImage.js"
 
 describe('hasMainImageProperClass', () => {
-    it('check if MainImage component is including mainImage__container class', () => {
+    it('check if MainImage div component is including mainImage__container class', () => {
         const mainImage = MainImage(); 
         expect(mainImage.classList).toContain("mainImage__container");
       });
-    });
-    
+    it('check if MainImage image component is including mainImage__image class', () => {
+        const mainImage = MainImage(); 
+        expect(mainImage.classList).toContain("mainImage__image");
+      });
+  });
+  
