fc88114c26e84824acfe32026c262f9d4d70f4eb
Piotr Rynio <41823689+PiotrRynio@users.noreply.github.com>
2021-12-07 01:37:21 +0100

    Revert "Add rendering component function (#60)"
    This reverts commit c94be07c477c5074d29dfdcc8b5c1f85dea28c07.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-};
-
-module.exports = config;
--- src/testsUtilities/renderComponent.js
+++
@@ -1,7 +0,0 @@
-export function renderComponent(component, testId = 'testId') {
-  document.body.innerHTML = `<div id="component-parent"></div>`;
-  const parent = document.querySelector('#component-parent');
-  component.id = testId;
-  parent.appendChild(component);
-  return component;
-}
