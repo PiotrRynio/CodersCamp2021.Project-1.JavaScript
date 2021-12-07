5d4e66c3af38dbf031dadd2525f22359c6af494e
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-07 20:14:33 +0100

    add tests

--- src/components/MainImage.test.js
+++ src/components/MainImage.test.js
@@ -1,13 +1,18 @@
-import MainImage from './MainImage';
+import { renderComponent } from '../testsUtilities/renderComponent';
+import MainImage, { defaultImage } from './MainImage';
 
 describe('hasMainImageProperClass', () => {
   it('check if MainImage div component is including mainImage__container class', () => {
+    renderComponent(MainImage());
+
     const mainImage = MainImage();
     expect(mainImage.classList).toContain('mainImage__container');
   });
 
-  it('check if MainImage image component is including mainImage__image class', () => {
+  it('check if MainImage image component is showing a proper content (photo)', () => {
+    renderComponent(MainImage());
+
     const mainImage = MainImage();
-    expect(mainImage.classList).toContain('mainImage__image');
+    expect(mainImage.firstChild.src).toEqual(defaultImage);
   });
 });
