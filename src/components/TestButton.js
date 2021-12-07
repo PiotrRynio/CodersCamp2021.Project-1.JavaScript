c94be07c477c5074d29dfdcc8b5c1f85dea28c07
Piotr Rynio <41823689+PiotrRynio@users.noreply.github.com>
2021-12-07 01:31:12 +0100

    Add rendering component function (#60)

--- src/components/TestButton.js
+++
@@ -1,9 +0,0 @@
-const TestButton = () => {
-  const testButton = document.createElement('button');
-  testButton.classList.add('testButton');
-  testButton.innerText = "test button"
-
-  return testButton;
-};
-
-export default TestButton;
