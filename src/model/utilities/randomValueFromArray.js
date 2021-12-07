dbb0e7867f7d0e5150616e61c303bd2f7ef5071a
Piotr Rynio <41823689+PiotrRynio@users.noreply.github.com>
2021-12-07 23:33:23 +0100

    Project settings (#64)

--- src/model/utilities/randomValueFromArray.js
+++ src/model/utilities/randomValueFromArray.js
@@ -1,6 +1,6 @@
 const randomValueFromArray = (valueArray) => {
   const randomArrayIndex = Math.floor(Math.random() * valueArray.length);
   return valueArray[randomArrayIndex];
-}
+};
 
 export default randomValueFromArray;
--- src/model/utilities/randomValueFromArray.test.js
+++ src/model/utilities/randomValueFromArray.test.js
@@ -1,17 +1,17 @@
 import randomValueFromArray from './randomValueFromArray';
 
 describe('randomValueFromArray', () => {
-  afterEach(()=> {
+  afterEach(() => {
     jest.spyOn(global.Math, 'random').mockRestore();
-  })
+  });
 
-  test("If we give it array, thats return it's value.",() => {
+  test("If we give it array, thats return it's value.", () => {
     const testArray = [1, 2, 3, 4];
     const result = randomValueFromArray(testArray);
     expect(testArray).toContain(result);
-  })
+  });
 
-  test("If random function returns 0 then function should return first value", () => {
+  test('If random function returns 0 then function should return first value', () => {
     jest.spyOn(global.Math, 'random').mockReturnValue(0);
 
     const testArray = [1, 2, 3, 4];
@@ -20,12 +20,12 @@ describe('randomValueFromArray', () => {
     expect(result).toBe(testArray[0]);
   });
 
-  test("If random function returns almost 1 then function should return last value", () => {
+  test('If random function returns almost 1 then function should return last value', () => {
     jest.spyOn(global.Math, 'random').mockReturnValue(0.999999);
 
     const testArray = [1, 2, 3, 4];
     const result = randomValueFromArray(testArray);
 
-    expect(result).toBe(testArray[testArray.length-1]);
+    expect(result).toBe(testArray[testArray.length - 1]);
   });
-})
+});
