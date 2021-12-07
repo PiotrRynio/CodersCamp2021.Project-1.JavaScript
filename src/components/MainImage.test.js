18986c789e5eb858d9f2905e9483eb9fbd344f94
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-07 19:17:00 +0100

    fix cosmetics

--- src/components/MainImage.test.js
+++ src/components/MainImage.test.js
@@ -1,13 +1,13 @@
-import MainImage from "./MainImage.js"
+import MainImage from './MainImage';
 
 describe('hasMainImageProperClass', () => {
-    it('check if MainImage div component is including mainImage__container class', () => {
-        const mainImage = MainImage(); 
-        expect(mainImage.classList).toContain("mainImage__container");
-      });
-    it('check if MainImage image component is including mainImage__image class', () => {
-        const mainImage = MainImage(); 
-        expect(mainImage.classList).toContain("mainImage__image");
-      });
+  it('check if MainImage div component is including mainImage__container class', () => {
+    const mainImage = MainImage();
+    expect(mainImage.classList).toContain('mainImage__container');
   });
-  
+
+  it('check if MainImage image component is including mainImage__image class', () => {
+    const mainImage = MainImage();
+    expect(mainImage.classList).toContain('mainImage__image');
+  });
+});
