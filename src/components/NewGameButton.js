1d391b3f2a15d5f29b7f8624e695dd772e6dd823
Radoslaw Dulisz <radek@dulisz.com>
2021-12-08 01:08:06 +0100

    added callback, first letter and new styles

--- src/components/NewGameButton.js
+++ src/components/NewGameButton.js
@@ -1,14 +1,18 @@
 const NewGameButton = (onButtonClick) => {
   const button = document.createElement('button');
-  const span = document.createElement('span');
+  const firstLetter = document.createElement('firstletter');
+  const elementNum = document.createElement('span');
 
   button.classList.add('newGameButton');
-  span.classList.add('newGameButton__greenBlock');
+  firstLetter.classList.add('newGameButton__first-letter');
+  elementNum.classList.add('elementNum');
 
   button.textContent = 'ew Game';
-  span.textContent = 'N';
+  firstLetter.textContent = 'N';
+  elementNum.textContent = '7';
 
-  button.appendChild(span);
+  button.appendChild(firstLetter);
+  firstLetter.appendChild(elementNum);
 
   button.addEventListener('click', onButtonClick);
 
