70895ad1133b418d2a439dcb6bd91e3b2e049f51
d0mbrow <88898014+BartoszDombrow@users.noreply.github.com>
2021-12-08 11:51:53 +0100

    Logika/gracz   człowiek (#47)
    * fix wrapper appearance
    
    * add player.js
    
    * add tests for Player.js
    
    * fixed test
    
    * fix code and change Player to player
    
    * fix small problems

---
+++ src/model/utilities/Player.test.js
@@ -0,0 +1,29 @@
+import player from './Player';
+
+describe('Player', () => {
+  const testPlayer = player();
+  const testQuestion = {
+    answers: ["Declan's men and his cook.", 'Los Pollos driver', 'No-Doze', 'Max Arciniega'],
+    correctAnswer: 'No-Doze',
+    questionObject: 'Beaten to death.',
+  };
+
+  it('callback should be called once if we ask the player a question', () => {
+    const mockCallback = jest.fn();
+    testPlayer.askQuestion(mockCallback, testQuestion);
+
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
+
+  it('callback should be called once if the player answers', () => {
+    const mockCallback = jest.fn();
+    testPlayer.answer(mockCallback, testQuestion);
+    expect(mockCallback.mock.calls.length).toBe(1);
+  });
+
+  it('callback should be called with testQuestion as an argument', () => {
+    const mockCallback = jest.fn();
+    testPlayer.answer(mockCallback, testQuestion);
+    expect(mockCallback.mock.calls[0][0]).toBe(testQuestion);
+  });
+});
