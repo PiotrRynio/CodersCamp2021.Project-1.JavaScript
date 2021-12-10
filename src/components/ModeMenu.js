3a118a9a4440ca41c27101c962d324e3ac84c36d
Patryk Święickci <patswiecicki@gmail.com>
2021-12-10 15:33:18 +0100

    Revert last commit

--- src/components/ModeMenu.js
+++ src/components/ModeMenu.js
@@ -1,12 +1,14 @@
-import ModeMenuButton from "./ModeMenuButton";
+import ModeMenuButton from './ModeMenuButton';
 
 const ModeMenu = (onButtonClick) => {
-    const section = document.createElement('div');
-    section.classList.add('modeMenu');
+  const section = document.createElement('div');
+  section.classList.add('modeMenu');
 
-    section.append(['Characters', 'Quotes', 'Deaths'].map( buttonName => ModeMenuButton(buttonName, onButtonClick)));
-      
-    return section;
-  };
-  export default ModeMenu;
-  
+  ['Characters', 'Quotes', 'Deaths'].map((buttonName) => {
+    let newButton = ModeMenuButton(buttonName, onButtonClick);
+    section.appendChild(newButton);
+  });
+
+  return section;
+};
+export default ModeMenu;
