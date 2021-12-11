5db9f1ca1a5e7fbbdbd1ffb173289d0acd8ff1b9
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-11 17:41:11 +0100

    add popup

---
+++ src/components/Modal.js
@@ -0,0 +1,8 @@
+const Modal = () => {
+  const modalPopup = document.createElement('div');
+
+  modalPopup.classList.add('modalPopup');
+
+  return modalPopup;
+};
+export default Modal;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './scss/style.scss';
 import Wrapper from './layouts/MainWrapper';
+import Modal from './components/Modal';
 
 function main() {
   document.querySelector('#app').appendChild(Wrapper());
+  document.querySelector('#app').appendChild(Modal());
 }
 
 main();
