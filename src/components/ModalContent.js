7c7246bb67d0eae61a1ae0fa4c2a14acce1f1e70
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-11 18:59:13 +0100

    Add showResults method

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,3 +1,11 @@
-const ModalContent = (PlayerAnswers, NpcAnswers) => {};
+const ModalContent = (PlayerAnswers, NpcAnswers) => {
+  const modalContent = document.createElement('b');
+
+  modalContent.showResults = () => {
+    const message =
+      'Czystość Twoich wyników wynosi XX%. Zająłeś XX miejsce w rankingu. Konkurencja uzyskała XX%. Jak się nazywasz?';
+    return message;
+  };
+};
 
 export default ModalContent;
