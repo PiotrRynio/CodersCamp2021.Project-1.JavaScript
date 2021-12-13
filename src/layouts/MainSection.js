5aa9a8e68e7c37cad598b3d93c8960601e7317be
Patryk Święickci <patswiecicki@gmail.com>
2021-12-13 20:42:51 +0100

    Add menu styles

--- src/layouts/MainSection.js
+++ src/layouts/MainSection.js
@@ -7,13 +7,28 @@ const MainSection = () => {
   mainSection.classList.add('mainSection');
 
   const menuSection = document.createElement('section');
+  menuSection.classList.add('menuSection');
   const contentSection = document.createElement('div');
 
   const startGame = () => {
     console.log('The game has started');
   };
 
-  menuSection.append(NewGameButton(startGame));
+  const rulesRankChange = (e) => {
+    console.log(e);
+    if (e.target.textContent === 'Rules')
+      e.target.innerHTML =
+        '<span class="newGameButton__firstLetter">H</span><span class="newGameButton__restOfText">all Of Fame</span>';
+    else if (e.target.textContent === 'Hall Of Fame')
+      e.target.innerHTML =
+        '<span class="newGameButton__firstLetter">R</span><span class="newGameButton__restOfText">ules</span>';
+  };
+
+  const rulesRankButton = NewGameButton(rulesRankChange);
+  rulesRankButton.innerHTML =
+    '<span class="newGameButton__firstLetter">H</span><span class="newGameButton__restOfText">all Of Fame</span>';
+
+  menuSection.append(NewGameButton(startGame), rulesRankButton);
   //TODO: Add Rules/HallOfFame button
 
   mainSection.append(menuSection);
