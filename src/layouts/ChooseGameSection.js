5b77932671cbe42bf1ed2916239d45582dffd0e3
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2021-12-14 20:43:35 +0100

    UI/menu component (#59)
    * Added mainMenu component
    
    * added sample callback
    
    * Changed buttons to components, added callbacks
    
    * added hover selector to buttons
    
    * after CR corrections
    
    * after CR corrections
    
    * The very last CR corrects
    
    * The very very last CR corrects
    
    * rename importing in style.scss
    
    * Rename mainMenu.js to MainMenu.js
    
    * Rename mainMenuButton.js to MainMenuButton.js
    
    * Update style.scss
    
    * renamed component in style.css
    
    * after CR corrections
    
    * Update src/components/ModeMenu.js
    
    * Revert last commit
    
    * Added test if component shows properly
    
    * After test correction.
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>

--- src/layouts/ChooseGameSection.js
+++ src/layouts/ChooseGameSection.js
@@ -1,6 +1,15 @@
+import ModeMenu from "../components/ModeMenu"
+
 const ChooseGameSection = () => {
+  let choosedMode; 
+  function handleModeMenuButtonClick(buttonName){
+    choosedMode = buttonName; 
+  }
+  
   const chooseGame = document.createElement('section');
   chooseGame.classList.add('chooseGameSection');
+  chooseGame.appendChild(ModeMenu(handleModeMenuButtonClick)); 
   return chooseGame;
 };
+
 export default ChooseGameSection;
