6770fdb70b03de053a892b6b50db71f95cd42da4
Agnieszka Przybylowska <agnieszka.przybylowska@coderscrew.pl>
2021-12-14 20:41:11 +0100

    After test correction.

--- src/components/ModeMenu.test.js
+++ src/components/ModeMenu.test.js
@@ -1,5 +1,7 @@
+import { screen } from '@testing-library/dom';
 import ModeMenu from './ModeMenu';
 import { renderComponent } from '../testsUtilities/renderComponent';
+
 describe('ModeMenu', () => {
   it('Properly shows rendered component', () => {
     renderComponent(ModeMenu());
@@ -7,10 +9,8 @@ describe('ModeMenu', () => {
     const buttonList = [...document.querySelectorAll('.modeMenu__button')];
     expect(testedDiv).toBeInTheDocument();
     expect(buttonList.length === 3).toBeTruthy();
-    expect(
-      buttonList[0].textContent === 'Characters' &&
-        buttonList[1].textContent === 'Quotes' &&
-        buttonList[2].textContent === 'Deaths',
-    ).toBeTruthy();
+    expect(screen.getByText('Characters')).toBeTruthy();
+    expect(screen.getByText('Quotes')).toBeTruthy();
+    expect(screen.getByText('Deaths')).toBeTruthy();
   });
 });
