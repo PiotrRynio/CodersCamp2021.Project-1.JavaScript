6f759adedffb6587b7b898e0eb90c28bb201146a
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-17 18:35:31 +0100

    popup to be completed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,11 @@
+import ModalButton from './ModalButton';
+
 const Modal = () => {
   const modalPopup = document.createElement('div');
 
   modalPopup.classList.add('modalPopup');
+  modalPopup.appendChild(ModalButton());
+  ModalButton.addEventListener = ('click', modalPopup.classList.remove('modalPopup.active'));
 
   return modalPopup;
 };
---
+++ src/components/ModalButton.js
@@ -0,0 +1,6 @@
+const ModalButton = () => {
+  const modalButton = document.createElement('button');
+  modalButton.classList.add('modalButton');
+  return modalButton;
+};
+export default ModalButton;
