fe969634f606647e0fde07899af38dec761f95de
Patryk Święickci <patswiecicki@gmail.com>
2021-12-18 12:33:22 +0100

    Remove comments from MainSection

--- src/layouts/MainSection.js
+++ src/layouts/MainSection.js
@@ -7,18 +7,18 @@ const MainSection = () => {
   const mainSection = document.createElement('div');
   mainSection.classList.add('mainSection');
 
-  // const answers = ['Walter White', 'Jesse Pinkman', 'Hank Shrader', 'Saul Goodman'];
-  // const handler = (isCorrect, answer) => {
-  //   console.log(answer, isCorrect);
-  // };
+  const answers = ['Walter White', 'Jesse Pinkman', 'Hank Shrader', 'Saul Goodman'];
+  const handler = (isCorrect, answer) => {
+    console.log(answer, isCorrect);
+  };
 
-  // mainSection.append(AnswerSection({ answers, correctAnswer: 'Walter White' }, handler));
-  // const menuButtonClicked = () => {
-  //   console.log('Menu button clicked');
-  // };
-  // mainSection.append(MenuButton('New Game', menuButtonClicked));
-  // mainSection.append(MenuButton('Hall of Fame', menuButtonClicked));
-  // mainSection.appendChild(MainImage());
+  mainSection.append(AnswerSection({ answers, correctAnswer: 'Walter White' }, handler));
+  const menuButtonClicked = () => {
+    console.log('Menu button clicked');
+  };
+  mainSection.append(MenuButton('New Game', menuButtonClicked));
+  mainSection.append(MenuButton('Hall of Fame', menuButtonClicked));
+  mainSection.appendChild(MainImage());
 
   mainSection.append(RankSection('Characters'));
 
