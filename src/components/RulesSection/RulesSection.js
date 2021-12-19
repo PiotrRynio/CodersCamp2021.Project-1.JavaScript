7dcca21eefb9dea49a6ae3eeabb15292f6316ba8
Patryk Święickci <patswiecicki@gmail.com>
2021-12-19 15:25:49 +0100

    Add rules object

---
+++ src/components/RulesSection/RulesSection.js
@@ -0,0 +1,29 @@
+const RulesSection = (rulesObject) => {
+  const rulesSection = document.createElement('section');
+  rulesSection.classList.add('rulesSection');
+
+  const category = document.createElement('h2');
+  category.classList.add('rulesSection__category');
+  const question = document.createElement('h2');
+  question.classList.add('rulesSection__question');
+  const rules = document.createElement('p');
+  rules.classList.add('rulesSection__rules');
+
+  const allRules = (whatToSelect) => {
+    return `You have one minute to answer fifteen questions. During the game on each question you need to select ${whatToSelect} The correct answer will be shown to you after giving the answer. If you want to go to the next question then click again.`;
+  };
+
+  rulesSection.changeRules = (newRulesObject) => {
+    category.textContent = newRulesObject.category;
+    question.textContent = newRulesObject.question;
+    rules.textContent = allRules(newRulesObject.specialRule);
+  };
+
+  rulesSection.changeRules(rulesObject);
+
+  rulesSection.append(category, question, rules);
+
+  return rulesSection;
+};
+
+export default RulesSection;
---
+++ src/model/constants.js
@@ -0,0 +1,17 @@
+export const RULES = {
+  Characters: {
+    category: 'Characters',
+    question: 'Who is this character?',
+    specialRule: 'who from Breaking Bad is shown on the photo.',
+  },
+  Quotes: {
+    category: 'Quotes',
+    question: 'Who said this quote?',
+    specialRule: "who from Breaking Bad said quote's words.",
+  },
+  Deaths: {
+    category: 'Deaths',
+    question: 'Who died this way?',
+    specialRule: 'who from Breaking Bad died in described way.',
+  },
+};
