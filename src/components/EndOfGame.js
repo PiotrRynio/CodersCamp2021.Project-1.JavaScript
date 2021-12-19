3da779f5636aa2ef147421aaefb5e3aa08ab8953
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-19 18:30:50 +0100

    Add player rank

--- src/components/EndOfGame.js
+++ src/components/EndOfGame.js
@@ -1,11 +1,15 @@
 import { saveScore, getScores } from '../model/saveScore';
 
 const EndOfGameModalContent = (answersListPlayer, answersListComputer) => {
-  const endOfGameModalContent = document.createElement('b');
+  const endOfGameModalContent = document.createElement('P');
 
   const playerType = {
     Player: answersListPlayer,
     Computer: answersListComputer,
+  };
+
+  const gameType = {
+    Character: 'Character',
   };
 
   const getPoints = (playerType) => {
@@ -16,14 +20,20 @@ const EndOfGameModalContent = (answersListPlayer, answersListComputer) => {
     return (getPoints(playerType) / playerType.length) * 100;
   };
 
+  const getPlaceFromHistoryRank = (gameType, points) => {
+    const scores = getScores(gameType);
+    const scoresBetterThanCurrent = scores.filter((entry) => entry.score > points).length + 1;
+    return scoresBetterThanCurrent;
+  };
+
   endOfGameModalContent.showResults = () => {
     const message = `Czystość Twoich wyników wynosi ${getPointsInPercentage(playerType.Player)}%. 
-    Zająłeś XX miejsce w rankingu. 
+    Zająłeś ${getPlaceFromHistoryRank(gameType.Character)} miejsce w rankingu. 
     Konkurencja uzyskała ${getPointsInPercentage(playerType.Computer)}%. 
     Jak się nazywasz?`;
     return message;
   };
-  console.log(endOfGameModalContent.showResults());
+  const textNode = document.createTextNode(endOfGameModalContent.showResults());
 
   const input = document.createElement('input');
   input.type = 'text';
@@ -37,28 +47,36 @@ const EndOfGameModalContent = (answersListPlayer, answersListComputer) => {
   acceptShowResultsButton.classList.add('endOfGameButton', 'acceptShowResultsButton');
   acceptShowResultsButton.innerText = 'Accept and show results';
 
-  const handleSaveScore = (name, score) => {
-    saveScore('Character', name, score); // TODO Handle other types of game
+  const handleSaveScore = (gameType, name, score) => {
+    if (name.length === 0) {
+      alert('Name can not be empty. Please enter your name.');
+      return false;
+    }
+    console.log(gameType, name, score);
+    return saveScore(gameType, name, score);
   };
 
-  const handleButtonAcceptAndEnd = (name, score) => {
-    acceptEndButton.innerText = `${name}, ${score}`;
-    handleSaveScore(name, score);
+  const handleButtonAcceptAndEnd = (gameType, name, score) => {
+    handleSaveScore(gameType, name, score);
   };
 
-  const handleButtonAcceptAndShowResults = (name, score) => {
-    acceptShowResultsButton.innerText = `${name}, ${score}`;
-    handleSaveScore(name, score);
+  const handleButtonAcceptAndShowResults = (gameType, name, score) => {
+    handleSaveScore(gameType, name, score);
   };
 
   acceptEndButton.addEventListener('click', () =>
-    handleButtonAcceptAndEnd(input.value, getPoints(playerType.Player)),
+    handleButtonAcceptAndEnd(gameType.Character, input.value, getPoints(playerType.Player)),
   );
   acceptShowResultsButton.addEventListener('click', () =>
-    handleButtonAcceptAndShowResults(input.value, getPoints(playerType.Computer)),
+    handleButtonAcceptAndShowResults(
+      gameType.Character,
+      input.value,
+      getPoints(playerType.Computer),
+    ),
   );
 
   //document.querySelector('.modalPopup').appendChild(input); // ModalPopup is not imported yet
+  document.querySelector('#app').appendChild(textNode);
   document.querySelector('#app').appendChild(input);
   document.querySelector('#app').appendChild(acceptEndButton);
   document.querySelector('#app').appendChild(acceptShowResultsButton);
@@ -84,7 +102,12 @@ const answer4 = {
   isCorrect: true,
 };
 
-const answersListPlayer = [answer1, answer2];
+const answer5 = {
+  answer: 'Travis',
+  isCorrect: true,
+};
+
+const answersListPlayer = [answer1, answer2, answer3, answer4, answer5];
 const answersListComputer = [answer3, answer4];
 
 EndOfGameModalContent(answersListPlayer, answersListComputer);
