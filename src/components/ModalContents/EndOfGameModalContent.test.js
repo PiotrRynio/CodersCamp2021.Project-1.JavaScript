b49ea33977b259cec62744997a6badf310679fd0
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-22 23:55:24 +0100

    Co-authored-by: kubajaro <kubajaro@users.noreply.github.com> Co-authored-by: Piotr Rynio <PiotrRynio@users.noreply.github.com> Co-authored-by: PSwiecicki <PSwiecicki@users.noreply.github.com>

---
+++ src/components/ModalContents/EndOfGameModalContent.test.js
@@ -0,0 +1,24 @@
+// import { renderComponent } from '../testsUtilities/renderComponent';
+// import EndOfGameModalContent from './EndOfGame';
+
+// describe('EndOfGameModalContent', () => {
+//   it('Content should be shown propery when first place', () => {
+//     renderComponent(EndOfGameModalContent());
+
+//     const answers = [
+//       { answer: 'TestNameA', isCorrect: true },
+//       { answer: 'TestNameB', isCorrect: true },
+//     ];
+//     //expect(EndOfGameModalContent(answers)).toBe('A');
+//   });
+
+//   it('Content should be shown propery when last place', () => {
+//     renderComponent(EndOfGameModalContent());
+
+//     const answers = [
+//       { answer: 'TestNameA', isCorrect: false },
+//       { answer: 'TestNameB', isCorrect: false },
+//     ];
+//     //expect(EndOfGameModalContent(answers)).toBe('A');
+//   });
+// });
