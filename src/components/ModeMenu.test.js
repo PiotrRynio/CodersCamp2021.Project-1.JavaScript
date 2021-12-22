bc792220a2a651535d52c8235cf6d0c258b90681
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2021-12-22 13:25:29 +0100

    add tests to ModeMenu

--- src/components/ModeMenu.test.js
+++ src/components/ModeMenu.test.js
@@ -6,11 +6,24 @@ describe('ModeMenu', () => {
   it('Properly shows rendered component', () => {
     renderComponent(ModeMenu());
     const testedDiv = document.querySelector('.modeMenu');
-    const buttonList = [...document.querySelectorAll('.modeMenu__button')];
+    const buttonList = document.querySelectorAll('.modeMenu__button');
+    const activeButton = document.querySelector('.modeMenu__button--active');
     expect(testedDiv).toBeInTheDocument();
     expect(buttonList.length === 3).toBeTruthy();
     expect(screen.getByText('Characters')).toBeTruthy();
     expect(screen.getByText('Quotes')).toBeTruthy();
     expect(screen.getByText('Deaths')).toBeTruthy();
+    expect(activeButton).toBeInTheDocument();
+  });
+
+  it('should remove active class in every button', () => {
+    renderComponent(ModeMenu());
+    const testedDiv = document.querySelector('.modeMenu');
+    const buttonList = document.querySelectorAll('.modeMenu__button');
+    testedDiv.removeActive();
+
+    buttonList.forEach((button) => {
+      expect(button).not.toHaveClass('modeMenu__button--active');
+    });
   });
 });
