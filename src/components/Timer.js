092ffa200853e7b1325e095ded4490d03548df57
kuba jarosz <kuba.jarooosz@gmail.com>
2021-12-22 20:37:02 +0100

    timer component changes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,24 +1,22 @@
-import { CHEMICAL_ELEMENTS } from '../model/chemicalElementsSymbols';
+import { chemicalElementsSymbols } from '../model/chemicalElementsSymbols';
 
 const Timer = () => {
   const timerElement = document.createElement('div');
   timerElement.classList.add('timer');
 
   const numberSection = document.createElement('div');
-  numberSection.textContent = '60';
   numberSection.classList.add('timer__number');
 
   timerElement.appendChild(numberSection);
 
   const textSection = document.createElement('div');
-  textSection.textContent = 'Nd';
   textSection.classList.add('timer__text');
 
   timerElement.appendChild(textSection);
 
   timerElement.updateTime = (secondsLeftInGame) => {
     numberSection.textContent = secondsLeftInGame;
-    textSection.textContent = CHEMICAL_ELEMENTS[secondsLeftInGame - 1];
+    textSection.textContent = chemicalElementsSymbols[secondsLeftInGame - 1];
   };
 
   return timerElement;
