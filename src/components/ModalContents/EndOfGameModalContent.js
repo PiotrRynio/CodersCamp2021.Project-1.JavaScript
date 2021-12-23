ab9bc0b7aa08cd9023f277b9117dfbecbba53f17
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2021-12-23 13:12:41 +0100

    Translate messages

--- src/components/ModalContents/EndOfGameModalContent.js
+++ src/components/ModalContents/EndOfGameModalContent.js
@@ -31,10 +31,12 @@ const EndOfGameModalContent = (gameType, answersListPlayer, answersListComputer)
   };
 
   const showResults = () => {
-    const message = `Czystość Twoich wyników wynosi ${getPointsInPercentage(answersListPlayer)}%.
-    Zająłeś ${getPlaceFromHistoryRank(getPoints(answersListPlayer)) + 1} miejsce w rankingu.
-    Konkurencjny dealer uzyskał w tym czasie ${getPointsInPercentage(answersListComputer)}%.
-    Jak się nazywasz?`;
+    const message = `The purity of your results is ${getPointsInPercentage(answersListPlayer)}%.
+    You've taken the # ${
+      getPlaceFromHistoryRank(getPoints(answersListPlayer)) + 1
+    } spot in the rankings. A competing dealer scored ${getPointsInPercentage(
+      answersListComputer,
+    )}% during this time. What is your name?`;
     return message;
   };
 
@@ -60,7 +62,7 @@ const EndOfGameModalContent = (gameType, answersListPlayer, answersListComputer)
 
   const handleSaveScore = (name, score) => {
     if (name.length === 0) {
-      alert('Nie możesz ciągle działać pod przykrywką. Podaj swój pseudonim!');
+      alert("You can't go undercover all the time. Give us your name!");
       return false;
     }
     console.log('Score saved:', gameType, name, score);
