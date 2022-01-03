7ac2e9c44ae42ce2f1e4e126d0c7f9169793f216
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2022-01-03 15:39:24 +0100

    Fix unit tests

--- src/components/ModalContents/EndOfGameModalContent.test.js
+++ src/components/ModalContents/EndOfGameModalContent.test.js
@@ -3,7 +3,7 @@ import EndOfGameModalContent from './EndOfGameModalContent';
 import { GAME_MODE } from '../../model/constants';
 
 describe('EndOfGameModalContent', () => {
-  it('Content should be shown propery when first place', () => {
+  it('should show modal', () => {
     const answers = [
       { answer: 'TestNameA', isCorrect: true },
       { answer: 'TestNameB', isCorrect: true },
@@ -15,13 +15,25 @@ describe('EndOfGameModalContent', () => {
     expect(modalContent).toBeInTheDocument();
   });
 
-  //   it('Content should be shown propery when last place', () => {
-  //     renderComponent(EndOfGameModalContent());
+  it('should calculate wrong answers as 0%', () => {
+    const answers = [
+      { answer: 'TestNameA', isCorrect: false },
+      { answer: 'TestNameB', isCorrect: false },
+    ];
+    renderComponent(EndOfGameModalContent(GAME_MODE.CHARACTERS, answers, answers));
 
-  //     const answers = [
-  //       { answer: 'TestNameA', isCorrect: false },
-  //       { answer: 'TestNameB', isCorrect: false },
-  //     ];
-  //     //expect(EndOfGameModalContent(answers)).toBe('A');
-  //   });
+    const statsMessage = document.querySelector('.gameStats').textContent;
+    expect(statsMessage).toContain('The purity of your results is 0%');
+  });
+
+  it('should calculate good answers as 100%', () => {
+    const answers = [
+      { answer: 'TestNameA', isCorrect: true },
+      { answer: 'TestNameB', isCorrect: true },
+    ];
+    renderComponent(EndOfGameModalContent(GAME_MODE.CHARACTERS, answers, answers));
+
+    const statsMessage = document.querySelector('.gameStats').textContent;
+    expect(statsMessage).toContain('The purity of your results is 100%');
+  });
 });
